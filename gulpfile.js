c073644d73d8424e90ae8ada3f1dca0d7bc5b297
andrewz-design <andrew.zamkovenko@gmail.com>
2021-09-29 02:38:46 +0300

    rework directory structure

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,12 @@
 const extend = require('deep-extend');
 const fs = require('fs');
 const {
-  dest,
-  series,
-  parallel,
-  src,
-  task,
-  watch
+    dest,
+    series,
+    parallel,
+    src,
+    task,
+    watch
 } = require('gulp');
 const gulpAutoprefixer = require('gulp-autoprefixer');
 const path = require('path');
@@ -18,94 +18,92 @@ const webpackStream = require('webpack-stream');
 const browserSync = require('browser-sync').create();
 
 const config = {
-  JS_SOURCE_DIR: './source/js/composite/',
-  JS_SOURCES: [
-    './partials/**/*.js',
-    './source/js/**/*.js',
-  ],
-  JS_OUT_DIR: './dist/js/composite/',
-  JS_OPTIONS: {
-    uglify: {
-      mangle: false
-    }
-  },
-  SASS_SOURCE_DIR: './source/sass/composite/**/*.{sass,scss}',
-  SASS_SOURCES: [
-    './partials/**/*.{sass,scss}',
-    './source/sass/**/*.{sass,scss}',
-  ],
-  SASS_OUT_DIR: './dist/css/composite/'
+    JS_SOURCE_DIR: './source/js/',
+    JS_SOURCES: [
+        './source/js/**/*.js',
+    ],
+    JS_OUT_DIR: './dist/js/',
+    JS_OPTIONS: {
+        uglify: {
+            mangle: false
+        }
+    },
+    SASS_SOURCE_DIR: './source/sass/**/*.{sass,scss}',
+    SASS_SOURCES: [
+        './source/sass/**/*.{sass,scss}',
+    ],
+    SASS_OUT_DIR: './dist/css/'
 };
 
 const jsFiles = readdirRecursive(config.JS_SOURCE_DIR);
 const entry = {};
 jsFiles.forEach(function(value) {
-  if (value.endsWith('.js')) {
-    const key = value.substring(0, value.length - 3);
-    entry[key] = config.JS_SOURCE_DIR + value;
-  }
+    if (value.endsWith('.js')) {
+        const key = value.substring(0, value.length - 3);
+        entry[key] = config.JS_SOURCE_DIR + value;
+    }
 });
 
 const webpackConfig = {
-  entry: entry,
-  mode: 'development',
-  output: {
-    path: path.resolve(__dirname, config.JS_OUT_DIR),
-    filename: '[name].min.js'
-  }
+    entry: entry,
+    mode: 'development',
+    output: {
+        path: path.resolve(__dirname, config.JS_OUT_DIR),
+        filename: '[name].min.js'
+    }
 };
 
 const webpackProdConfig = extend({}, webpackConfig, {
-  mode: 'production',
+    mode: 'production',
 });
 
 const webpackWatchConfig = extend({}, webpackConfig, {
-  watch: true,
+    watch: true,
 });
 
 task('compile-js', function() {
-  return src(config.JS_SOURCES)
-    .pipe(webpackStream(
-      webpackProdConfig, webpack
-    ))
-    .pipe(dest(config.JS_OUT_DIR));
+    return src(config.JS_SOURCES)
+        .pipe(webpackStream(
+            webpackProdConfig, webpack
+        ))
+        .pipe(dest(config.JS_OUT_DIR));
 });
 
 task('compile-sass', function(cb) {
-  return src(config.SASS_SOURCE_DIR)
-    .pipe(sass({
-      outputStyle: 'compressed'
-    })).on('error', sass.logError)
-    .pipe(rename(function(path) {
-      path.basename += '.min';
-    }))
-    .pipe(gulpAutoprefixer())
-    .pipe(dest(config.SASS_OUT_DIR));
+    return src(config.SASS_SOURCE_DIR)
+        .pipe(sass({
+            outputStyle: 'compressed'
+        })).on('error', sass.logError)
+        .pipe(rename(function(path) {
+            path.basename += '.min';
+        }))
+        .pipe(gulpAutoprefixer())
+        .pipe(dest(config.SASS_OUT_DIR));
 });
 
 task('watch-sass', function() {
-  watch(config.SASS_SOURCES, series('compile-sass'));
+    watch(config.SASS_SOURCES, series('compile-sass'));
 });
 
 task('watch-js', function() {
-  src(config.JS_SOURCES)
-  .pipe(webpackStream(webpackWatchConfig, webpack))
-  .pipe(dest(config.JS_OUT_DIR));
+    src(config.JS_SOURCES)
+        .pipe(webpackStream(webpackWatchConfig, webpack))
+        .pipe(dest(config.JS_OUT_DIR));
 });
 
-task('browser-sync', function () {
-  browserSync.init({
-    proxy: "localhost:8080"
-  });
-  watch('./source/').on('change', browserSync.reload);
-  watch('./partials/').on('change', browserSync.reload);
-  watch('./content/').on('change', browserSync.reload);
-  watch('./views/').on('change', browserSync.reload);
-  watch('./static/').on('change', browserSync.reload);
+task('browser-sync', function() {
+    browserSync.init({
+        proxy: "localhost:8080"
+    });
+    watch('./source/').on('change', browserSync.reload);
+    watch('./partials/').on('change', browserSync.reload);
+    watch('./content/').on('change', browserSync.reload);
+    watch('./views/').on('change', browserSync.reload);
+    watch('./static/').on('change', browserSync.reload);
 });
 
 task('grow-build', parallel('compile-js', 'compile-sass'))
-//task('grow-run', parallel('compile-js', 'compile-sass'))
+    //task('grow-run', parallel('compile-js', 'compile-sass'))
 
 exports.build = parallel('compile-js', 'compile-sass')
 exports.default = series(parallel('watch-js', 'watch-sass', 'browser-sync'), 'compile-sass', 'compile-js')
